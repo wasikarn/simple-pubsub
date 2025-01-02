fd035a39ea835de0af72d17dbbf096ff7323f0fc
BIG <bigguitarsolo@gmail.com>
2025-01-02 11:54:46 +0700

    Add NestJS EventEmitterModule to AppModule
    Introduced `@nestjs/event-emitter` as a dependency and configured it in
    `AppModule`. This enables event-driven functionality within the
    application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [EventEmitterModule.forRoot()],
   providers: [AppService],
 })
 export class AppModule {}
